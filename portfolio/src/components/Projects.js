618b9b3ab8396206eb71041295aacd9313be035b
annierome1 <112560966+annierome1@users.noreply.github.com>
2025-01-14 21:32:16 -0500

    update

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -161,7 +161,7 @@ export default function Projects() {
 
         {/* Content based on active tab */}
         {activeTab === "completed" && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 gap-6">
             {completedProjects.map((project) => renderProject(project))}
           </div>
         )}
